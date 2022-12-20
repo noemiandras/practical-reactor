7b3ac5ef3a3caa8a7cad0ad1bcac99e18df38bf5
Noemi Andras <noemi.andras1@motorolasolutions.com>
2022-12-20 08:26:31 -0600

    solve broadcasting exercise file

--- exercises/src/test/java/c12_Broadcasting.java
+++ exercises/src/test/java/c12_Broadcasting.java
@@ -1,4 +1,5 @@
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
@@ -8,15 +9,15 @@ import java.util.concurrent.CopyOnWriteArrayList;
 /**
  * In this chapter we will learn difference between hot and cold publishers,
  * how to split a publisher into multiple and how to keep history so late subscriber don't miss any updates.
- *
+ * <p>
  * Read first:
- *
+ * <p>
  * https://projectreactor.io/docs/core/release/reference/#reactor.hotCold
  * https://projectreactor.io/docs/core/release/reference/#which.multicasting
  * https://projectreactor.io/docs/core/release/reference/#advanced-broadcast-multiple-subscribers-connectableflux
- *
+ * <p>
  * Useful documentation:
- *
+ * <p>
  * https://projectreactor.io/docs/core/release/reference/#which-operator
  * https://projectreactor.io/docs/core/release/api/reactor/core/publisher/Mono.html
  * https://projectreactor.io/docs/core/release/api/reactor/core/publisher/Flux.html
@@ -32,8 +33,7 @@ public class c12_Broadcasting extends BroadcastingBase {
     @Test
     public void sharing_is_caring() throws InterruptedException {
         Flux<Message> messages = messageStream()
-                //todo: do your changes here
-                ;
+                .publish().refCount(2);
 
         //don't change code below
         Flux<String> userStream = messages.map(m -> m.user);
@@ -49,29 +49,34 @@ public class c12_Broadcasting extends BroadcastingBase {
 
         Assertions.assertEquals(Arrays.asList("user#0", "user#1", "user#2", "user#3", "user#4"), metaData);
         Assertions.assertEquals(Arrays.asList("payload#0", "payload#1", "payload#2", "payload#3", "payload#4"),
-                                payload);
+                payload);
     }
 
     /**
      * Since two subscribers are interested in the updates, which are coming from same source, convert `updates` stream
      * to from cold to hot source.
      * Answer: What is the difference between hot and cold publisher? Why does won't .share() work in this case?
+     * <p>
+     * <p>
+     * Share would send already sent out messages upon subscription which we don't want.
+     * <p>
+     * Cold publisher only generates data when subscribed
+     * Hot generates before that. Subscriber only sees data after subscribed
      */
     @Test
     public void hot_vs_cold() {
         Flux<String> updates = systemUpdates()
-                //todo: do your changes here
-                ;
+                .publish().autoConnect();
 
         //subscriber 1
         StepVerifier.create(updates.take(3).doOnNext(n -> System.out.println("subscriber 1 got: " + n)))
-                    .expectNext("RESTARTED", "UNHEALTHY", "HEALTHY")
-                    .verifyComplete();
+                .expectNext("RESTARTED", "UNHEALTHY", "HEALTHY")
+                .verifyComplete();
 
         //subscriber 2
         StepVerifier.create(updates.take(4).doOnNext(n -> System.out.println("subscriber 2 got: " + n)))
-                    .expectNext("DISK_SPACE_LOW", "OOM_DETECTED", "CRASHED", "UNKNOWN")
-                    .verifyComplete();
+                .expectNext("DISK_SPACE_LOW", "OOM_DETECTED", "CRASHED", "UNKNOWN")
+                .verifyComplete();
     }
 
     /**
@@ -82,18 +87,17 @@ public class c12_Broadcasting extends BroadcastingBase {
     @Test
     public void history_lesson() {
         Flux<String> updates = systemUpdates()
-                //todo: do your changes here
-                ;
+                .share();
 
         //subscriber 1
         StepVerifier.create(updates.take(3).doOnNext(n -> System.out.println("subscriber 1 got: " + n)))
-                    .expectNext("RESTARTED", "UNHEALTHY", "HEALTHY")
-                    .verifyComplete();
+                .expectNext("RESTARTED", "UNHEALTHY", "HEALTHY")
+                .verifyComplete();
 
         //subscriber 2
         StepVerifier.create(updates.take(5).doOnNext(n -> System.out.println("subscriber 2 got: " + n)))
-                    .expectNext("RESTARTED", "UNHEALTHY", "HEALTHY", "DISK_SPACE_LOW", "OOM_DETECTED")
-                    .verifyComplete();
+                .expectNext("RESTARTED", "UNHEALTHY", "HEALTHY", "DISK_SPACE_LOW", "OOM_DETECTED")
+                .verifyComplete();
     }
 
 }
